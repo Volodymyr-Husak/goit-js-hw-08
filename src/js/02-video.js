4d54cef458cd91fe9193841a7da494d0b8ff249d
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-19 23:49:10 +0300

    corrects task 02, add lodash.throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,76 @@
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', function (event) {
+//   player
+//     .getCurrentTime()
+//     .then(function (seconds) {
+//       // seconds = the current playback position
+//       //   console.log(seconds);
+//       return localStorage.setItem('videoplayer-current-time', seconds);
+//       //   return seconds;
+//     })
+//     .catch(function (error) {
+//       // an error occurred
+//       console.log('Помилка!!!!!');
+//     });
+// });
+
+// // console.log(localStorage.getItem('videoplayer-current-time'));
+// const videoPlayerCurrentTime = localStorage.getItem('videoplayer-current-time');
+// // ====================================перезавантаження сторінки===================================
+// player
+//   .setCurrentTime(videoPlayerCurrentTime)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
+
+//   ==========================================================================================================
+//   ==========================================Throttle================================================================
+//   ==========================================================================================================
+
 import Player from '@vimeo/player';
+// import Throttle from 'lodash.throttle';
+
+// const _ = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (event) {
-  player
-    .getCurrentTime()
-    .then(function (seconds) {
-      // seconds = the current playback position
-      //   console.log(seconds);
-      return localStorage.setItem('videoplayer-current-time', seconds);
-      //   return seconds;
-    })
-    .catch(function (error) {
-      // an error occurred
-      console.log('Помилка!!!!!');
-    });
-});
+player.on(
+  'timeupdate',
+  _.throttle(() => {
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        // seconds = the current playback position
+        // console.log(seconds);
+        return localStorage.setItem('videoplayer-current-time', seconds);
+        //   return seconds;
+      })
+      .catch(function (error) {
+        // an error occurred
+        console.log('Помилка!!!!!');
+      });
+  }, 1000)
+);
 
 // console.log(localStorage.getItem('videoplayer-current-time'));
 const videoPlayerCurrentTime = localStorage.getItem('videoplayer-current-time');
-// ====================================перезавантаження сторінки===================================
+// ====================================Перезавантаження сторінки===================================
 player
   .setCurrentTime(videoPlayerCurrentTime)
   .then(function (seconds) {
